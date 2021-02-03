2759e870264a44a8a42427c8648e004722846bf6
Eray Aydın <erayaydin2016@gmail.com>
2021-02-03 05:19:10 +0300

    Initialize homework 2 template

---
+++ app.php
@@ -0,0 +1,24 @@
+<?php
+
+$sex = "male"; // cinsiyet (male/female)
+$weight = 55; // kilo (kg)
+$height = 170; // boy (cm)
+$age = 25; // yaş (sene)
+
+/**
+ * Bu ödevde günlük kalori hesaplaması yapacağız.
+ * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
+ * kişinin günlük kaç kalori alması gerektiğini hesaplayıp
+ * bunu ekrana yazan PHP kodunu oluşturmamız gerekiyor.
+ * 
+ * Erkekler için; 10 x kilo + 6.25 x boy - 5 x yaş + 5
+ * Kadınlar için; 10 x kilo + 6.25 x boy - 5 x yaş - 161
+ * 
+ * Örneğin;
+ * $sex = "female";
+ * $weight = 60;
+ * $height = 170;
+ * $age = 30;
+ * 
+ * Ekrana 1326.5 yazması gerekiyor.
+ */
